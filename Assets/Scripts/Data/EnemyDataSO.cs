0a56a9fd992b8fd7e0e335b83a7166eda6899391
Facundo Gerry <facuto365pro@gmail.com>
2026-06-01 09:43:11 -0300

    Add pistol

--- Assets/Scripts/Data/EnemyDataSO.cs
+++ Assets/Scripts/Data/EnemyDataSO.cs
@@ -1,12 +1,12 @@
 ﻿using UnityEngine;
 
-[CreateAssetMenu(fileName = "EnemyData", menuName = "NPCs/EnemyData")]
+[CreateAssetMenu(fileName = "EnemyData", menuName = "NPC/EnemyData")]
 
 public class EnemyDataSO : ScriptableObject
 {
     public EnemyClasses enemyClass;
     public EnemyAttackType attackType;
     public float distanceToShoot;
+    public int shootingDamage;
     public bool canMove;
-    public GameObject objectToThrow;
 }
--- Assets/Scripts/Game/NPCs/BulletEnemy.cs
+++ Assets/Scripts/Game/NPCs/BulletEnemy.cs
@@ -1,8 +1,19 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(Collider))]
+
 public class BulletEnemy : MonoBehaviour, IPooleable
 {
+    [SerializeField] private EnemyDataSO _data;
+
     public bool IsActive { get; set; }
+
+    private void OnTriggerEnter(Collider coll)
+    {
+        if (coll.TryGetComponent(out IDamageable damage))
+            damage.TakeDamage(_data.shootingDamage);
+        DeActivate();
+    }
 
     public void Activate()
     {
